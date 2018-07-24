3d8f15d7626f23c742ec90c101f26cd74d5eeba5
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2018-07-24 14:35:56 +0300

    - Carthage support

---
+++ MMDrawerController/MMDrawerControllerFramework.h
@@ -0,0 +1,24 @@
+//
+//  MMDrawerController.h
+//  MMDrawerController
+//
+//  Created by mac-246 on 7/24/18.
+//  Copyright © 2018 Anton Plebanovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMDrawerController.
+FOUNDATION_EXPORT double MMDrawerControllerVersionNumber;
+
+//! Project version string for MMDrawerController.
+FOUNDATION_EXPORT const unsigned char MMDrawerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMDrawerController/PublicHeader.h>
+
+
+#import <MMDrawerController/MMDrawerBarButtonItem.h>
+#import <MMDrawerController/MMDrawerController+Subclass.h>
+#import <MMDrawerController/MMDrawerController.h>
+#import <MMDrawerController/MMDrawerVisualState.h>
+#import <MMDrawerController/UIViewController+MMDrawerController.h>
